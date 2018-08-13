d342457be17d9c90d557a381dd1587ed7002ae23
Tom Pickersgill <tom@picky.plus.com>
2018-08-13 22:35:18 +0100

    Added browser application and data upload functionality

--- database/database_tools.py
+++ database/database_tools.py
@@ -1,9 +1,6 @@
-import sqlite3
-from tableclasses import FPL_data_table
+import sqlite3, time, pdb
+from database.tableclasses import FPL_data_table
 from sqlalchemy import create_engine
-
-
-
 
 class DatabaseTools():
     """ A set of databse connection tools """
@@ -14,7 +11,7 @@ class DatabaseTools():
         self.table_name = table_name
         self.conn = sqlite3.connect(db_name)
         self.c = self.conn.cursor()
-        self.engine = create_engine('sqlite://', echo=False)
+        #self.engine = create_engine('sqlite://', echo=False)
         
         if table_name is 'FPL_data':
             self.table_class = FPL_data_table()
@@ -35,6 +32,30 @@ class DatabaseTools():
     def drop_table(self):
         self.c.execute('DROP TABLE IF EXISTS {}'.format(self.table_name))
 
+    def upload(self, df):
+        start = time.time()
+        df.to_sql(self.table_name, self.conn, if_exists='replace')
+        print('Time for upload: {}s'.format(time.time()-start))
+        pdb.set_trace()
+        self.conn.commit()
+        self.c.close()
+        self.conn.close()
 
+def create_connection(db_file):
+    from sqlite3 import Error
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None 
+
+create_connection(r'C:\Users\Tom\Python\southgate\database\Southgate_db.db')
 #create_table('FPL_data', FPL_data_table())
 #drop_table('FPL_data')
--- execution.py
+++ execution.py
@@ -2,15 +2,23 @@
 The execution file from which all data analysis is run
 """
 import pdb, importlib
-
+import pandas as pd
 from tools.ReadDataFromURL import DataTools
 from tools.MemoryOptimiser import type_reduct_dict
 from tools.DataAnalysis import DataAnalysisToolkit
 from database.database_tools import DatabaseTools
 
 DT = DataTools()
-df = DT.HistoricalData({'2016':'5-37', '2017':'0-37', '2018':'0-0'})
-DA = DataAnalysisToolkit(df)
+#df = DT.HistoricalData({'2016':'5-37', '2017':'0-37', '2018':'0-0'})
+
+#DA = DataAnalysisToolkit(df)
+df = pd.read_csv('gamelogs/FPL16-GW5.csv')
+
+DB = DatabaseTools(r'database/southgate_db.db','FPL_data')
+DB.upload(df)
+
+
+
 
 #df = DT.HistoricalData({'2016':'5-37', '2017':'0-37', '2018':'0-0'})
 #df_full = DT.LoadFullData()
